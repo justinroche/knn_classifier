2d76c93e5b4755581a9becd9cd0316f0eb3991e4
justinroche_ <justinroche03@gmail.com>
2024-01-24 10:10:09 -0600

    Adjusted fit and split methods

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import TextIO
 
 
 class KNNClassifier:
@@ -7,31 +8,36 @@ class KNNClassifier:
         self.data = None
         self.label = None
 
-    @staticmethod
-    def split_train_file(train_file):
-        try:
-            with open(train_file, "r") as file:
-                lines = file.readlines()
-
-                data = np.array(
-                    [list(map(float, line.split(",")[:-1])) for line in lines]
-                )
-
-                label = np.array(
-                    [int(line.split(",")[-1]) for line in lines]
-                )
-
-        except Exception as e:
-            print(type(e).__name__, e)
-
-        return data, label
-
-    def fit(self, train_data, train_label):
-        self.data = train_data
-        self.label = train_label
+    def fit(self, training_data, training_label):
+        self.data: np.ndarray = training_data
+        self.label: np.ndarray = training_label
 
     def predict(self, test_data):
         pass
 
     def check_accuracy(self):
         pass
+
+    @staticmethod
+    def __split_textio(file: TextIO):
+        lines = file.readlines()
+
+        data = np.array(
+            [list(map(float, line.split(",")[:-1])) for line in lines]
+        )
+
+        label = np.array(
+            [int(line.split(",")[-1]) for line in lines]
+        )
+
+        return data, label
+
+    @staticmethod
+    def split_train_file(train_file):
+        try:
+            with open(train_file, "r") as file:
+                return KNNClassifier.__split_textio(file)
+
+        except Exception as e:
+            print(type(e).__name__, e)
+            return None
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
     classifier = KNNClassifier()
 
     data, label = KNNClassifier.split_train_file("wine_train.txt")
-    print(data)
-    print(label)
+    classifier.fit(data, label)
+
 
 if __name__ == "__main__":
     main()
